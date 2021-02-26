6898bfe59eecff00678b0af438182e4c034064f8
Qubaisy Andiyantama <aqubayisi@yahoo.com>
2021-02-26 20:15:53 +0700

    CertificatePinner

--- core/src/main/java/com/mqa/jobwishlist/core/di/NetworkModule.kt
+++ core/src/main/java/com/mqa/jobwishlist/core/di/NetworkModule.kt
@@ -1,10 +1,15 @@
 package com.mqa.jobwishlist.core.di
 
 import com.mqa.jobwishlist.core.data.source.remote.network.ApiService
+import com.mqa.jobwishlist.core.utils.Constant.Companion.BASE_URL
+import com.mqa.jobwishlist.core.utils.Constant.Companion.HOSTNAME
+import com.mqa.jobwishlist.core.utils.Constant.Companion.PIN
+import com.mqa.jobwishlist.core.utils.Constant.Companion.USERNAME_BASIC_AUTH
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -17,18 +22,22 @@ class NetworkModule {
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val certificatePinner = CertificatePinner.Builder()
+            .add(HOSTNAME, PIN)
+            .build()
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-                .addInterceptor(BasicAuthInterceptor("94e78326-4186-42a7-be50-6e20d7b7b01e ",""))
+            .addInterceptor(BasicAuthInterceptor(USERNAME_BASIC_AUTH,""))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
     @Provides
     fun provideApiService(client: OkHttpClient): ApiService {
         val retrofit = Retrofit.Builder()
-            .baseUrl("https://www.reed.co.uk/api/1.0/")
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
             .build()
--- core/src/main/java/com/mqa/jobwishlist/core/utils/Constant.kt
+++ core/src/main/java/com/mqa/jobwishlist/core/utils/Constant.kt
@@ -3,5 +3,10 @@ package com.mqa.jobwishlist.core.utils
 class Constant {
     companion object {
         const val DEEPLINK_SEARCH = "job://search"
+
+        const val HOSTNAME = "reed.co.uk"
+        const val PIN = "sha256/zXcuOHO7HVN5BhUgt/JS6dXHJsNMh9GqlW8NQ3HSMh0="
+        const val BASE_URL = "https://www.reed.co.uk/api/1.0/"
+        const val USERNAME_BASIC_AUTH = "94e78326-4186-42a7-be50-6e20d7b7b01e "
     }
 }
