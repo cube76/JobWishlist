9f2d9497022b9907aca7297302a4589d15614a2b
Qubaisy Andiyantama <aqubayisi@yahoo.com>
2021-03-01 16:06:24 +0700

    unused resource fix

--- fullTime/src/main/java/com/mqa/jobwishlist/fulltime/FullTimeViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.mqa.jobwishlist.fulltime
-
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import com.mqa.jobwishlist.core.domain.usecase.JobUseCase
-
-class FullTimeViewModel(jobUseCase: JobUseCase) : ViewModel() {
-    val job = jobUseCase.getAllFullTimeJob().asLiveData()
-}
